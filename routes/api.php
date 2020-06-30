68446982bf5b48c93dd1d6f387dc3438c4914f43
Abdul-fatai <staticdev20046@gmail.com>
2020-06-30 12:28:03 +0100

    ready for deploy

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-
 /**  ADMIN ENDPOINS */
 Route::group(['prefix' => 'admin', 'middleware' => ['auth:api', 'role:admin']], function () {
 
